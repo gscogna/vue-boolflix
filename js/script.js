41e05f5b82f6063cc5d82592bbd2942f0d196415
Gennaro <scognamiglioge@hotmail.it>
2021-02-11 16:36:21 +0100

    Stelle

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ var app = new Vue({
         alterantiveFoto:'https://pbs.twimg.com/profile_images/1006975467042328583/FESn8ZMw_400x400.jpg',
         preselezione: 'https://image.tmdb.org/t/p/',
         formato: 'w342',
-        bandiere: [],
         contenuti:[],
         series:[],
         query:'',
         apikey:'9c849102cf7097b31d1314854e343536',
         lang: 'it-IT'
+    },
+    computed: {
+        filtered: function () {
+            return this.contenuti.slice(0, 2)
+        }
     },
     methods:{
         ricerca(){
@@ -37,27 +41,8 @@ var app = new Vue({
                     this.series.concat(result.data.results);
                 })
                 .catch((error) => console.log(error))
-        },
-
-        filtroBandiere(){
-            this.contenuti.forEach(element => {
-                if (this.bandiere.includes(element.original_language)) {
-                    this.bandiere.push(element.original_language);
-                }
-            });
-        }
+        },  
+        
     }
 });
-
-
-
-
-// 'img/es.jpeg', 'img/it.jpeg', 'img/fr.jpeg', 'img/en.jpeg', 'img/pt.jpeg'
-
-
-{/* <img class="bandiera-size" : src="(contenuto.original_language == 'es') ? bandiere[0] : (contenuto.original_language == 'it') ? bandiere[1] : (contenuto.original_language == 'fr') ? bandiere[2] : (contenuto.original_language == 'en') ? bandiere[3] : (contenuto.original_language == 'pt') ? bandiere [4] : (contenuto.original_language) " : alt="contenuto.original_language"> */}
-
-// stella a metà
-{/* <i class="far fa-star"></i> */}
-
 
