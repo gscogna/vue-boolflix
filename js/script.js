f8358c6f9fbde3e5c00e6d5a1c4b1be42dfa3949
Gennaro <scognamiglioge@hotmail.it>
2021-02-12 16:29:45 +0100

    modifiche col css

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 var app = new Vue({
     el:'#app',
     data:{
-        alterantiveFoto:'https://pbs.twimg.com/profile_images/1006975467042328583/FESn8ZMw_400x400.jpg',
+        alterantiveFoto:'img/genio.jpg',
         preselezione: 'https://image.tmdb.org/t/p/',
-        formato: 'w342',
+        formato: 'w500',
         contenuti:[],
         series:[],
         query:'',
         apikey:'9c849102cf7097b31d1314854e343536',
         lang: 'it-IT'
-    },
-    computed: {
-        filtered: function () {
-            return this.contenuti.slice(0, 2)
-        }
     },
     methods:{
         ricerca(){
@@ -41,7 +36,15 @@ var app = new Vue({
                     this.series.concat(result.data.results);
                 })
                 .catch((error) => console.log(error))
-        },  
+        },
+        
+        filtered (voto) {
+                return parseInt(voto / 2);
+        },
+
+        filteredStar (voto) {
+                return 5 - parseInt(voto / 2);
+            }  
         
     }
 });
